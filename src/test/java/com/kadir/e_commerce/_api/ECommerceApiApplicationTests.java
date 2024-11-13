4c9a07e6dc70fac58dfec854832617bff0992243
Kadir <84664117+VectortheGreat@users.noreply.github.com>
2024-11-14 00:21:10 +0300

    first commit

---
+++ src/test/java/com/kadir/e_commerce/_api/ECommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadir.e_commerce._api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
